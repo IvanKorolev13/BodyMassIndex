05b5ce3d0eda982325f9dcc3e85a7b3ed30eb0f6
IvanKorolev13 <ikorolev13@gmail.com>
2023-01-30 14:49:57 +0300

    corrected veriables' name

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(int h, int m) {
-        int index = m * 10000/ (h * h); //ИМТ = масс в кг/ квадрат роста в метрах,
+    public int calculate(int height, int mass) {
+        int index = mass * 10000/ (height * height); //ИМТ = масс в кг/ квадрат роста в метрах,
                                         // идеально использовать не целоечисленый тип, чтобы был результат с десятичными и сотыми
         return index;
     }
